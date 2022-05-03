32f253b00116960b8b1a74f2c3dd2101ff3799c0
Abhishek Ghosh <gabhiiaf93@gmail.com>
2022-05-03 21:53:14 +0530

    Add files via upload
    Adding handler for icons

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,7 +7,7 @@ import { loc, alphabets } from "./data";
 import { filterCitiesAndCountries, limitTheNumberOfCalls } from "./helper";
 import { ClearAllSelection } from "./components/clear-all";
 import { MainNavMenu } from "./components/main-nav-menu";
-
+import { RESIZE_TYPES } from "./constant";
 function App() {
   const optimizedHandler = useRef(null);
   // Maybe can use useContext and create a singluar state for these but not
@@ -26,6 +26,29 @@ function App() {
   const onSearchHandler = (searchParam) => {
     updateCityCountries(filterCitiesAndCountries(loc, searchParam));
   };
+  const onIconsClickHandler = (selectedElementVal) => {
+    switch (selectedElementVal) {
+      case RESIZE_TYPES.MAXIMIZE:
+        updateIsMinimised(false);
+        updateIsCollapsed(false);
+        break;
+      case RESIZE_TYPES.MINIMIZE:
+        updateIsMinimised(true);
+        updateIsCollapsed(false);
+        break;
+      case RESIZE_TYPES.COLLAPSE:
+        updateIsCollapsed(true);
+        updateIsMinimised(false);
+        break;
+      case RESIZE_TYPES.EXPAND:
+        updateIsCollapsed(false);
+        updateIsMinimised(false);
+        break;
+      default:
+        break;
+    }
+  };
+
   const onCheckboxChangeHandler = (selectedElement) => {
     const elementId = selectedElement.target.value;
     updateSelectedLoc({
@@ -38,26 +61,27 @@ function App() {
   };
   return (
     <div className="parent-container">
-      <MainNavMenu isMinimised={isMinimised} isCollapsed={isCollapsed} />
-      <MemoizedSearchBox onSearchHandler={optimizedHandler.currnet} />
-      <ClearAllSelection clearSelectionHandler={clearSelectionHandler} />
-      <div
-        className={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
-      >
-        <div>
-          <ListItems
-            cityCountries={cityCountries}
-            onCheckboxChangeHandler={onCheckboxChangeHandler}
-            selectedLoc={selectedLoc}
-          />
-        </div>
-        <div>{/* <AlphabetList alphabets={alphabets} /> */}</div>
-      </div>
+      <MainNavMenu
+        isMinimised={isMinimised}
+        isCollapsed={isCollapsed}
+        onClickHandler={onIconsClickHandler}
+      />
+      {!isMinimised && !isCollapsed && (
+        <>
+          <MemoizedSearchBox onSearchHandler={optimizedHandler.currnet} />
+          <ClearAllSelection clearSelectionHandler={clearSelectionHandler} />
+          <div className="listItems">
+            <div>
+              <ListItems
+                cityCountries={cityCountries}
+                onCheckboxChangeHandler={onCheckboxChangeHandler}
+                selectedLoc={selectedLoc}
+              />
+            </div>
+            <div>{/* <AlphabetList alphabets={alphabets} /> */}</div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/App/components/main-nav-menu/index.jsx
+++ src/App/components/main-nav-menu/index.jsx
@@ -1,19 +1,39 @@
 import "./main-nav.css";
-
+import { RESIZE_TYPES } from "../../constant";
 export const MainNavMenu = ({ isCollapsed, isMinimised, onClickHandler }) => {
   return (
     <div className="filters">
       <div>
         <h4>Locations</h4>
       </div>
-      <div>
+      <div className="icons">
         <div>
-          {!isMinimised && <i class="fa-solid fa-window-minimize"></i>}
-          {isMinimised && <i class="fa-thin fa-window-maximize"></i>}
+          {!isMinimised && !isCollapsed && (
+            <i
+              className="fa-solid fa-window-minimize"
+              onClick={() => onClickHandler(RESIZE_TYPES.MINIMIZE)}
+            ></i>
+          )}
+          {isMinimised && (
+            <i
+              className="fa-solid fa-window-maximize"
+              onClick={() => onClickHandler(RESIZE_TYPES.MAXIMIZE)}
+            ></i>
+          )}
         </div>
         <div>
-          <i class="fa-solid fa-arrow-left"></i>
-          <i class="fa-solid fa-arrow-right"></i>
+          {!isMinimised && !isCollapsed && (
+            <i
+              className="fa-solid fa-arrow-left"
+              onClick={() => onClickHandler(RESIZE_TYPES.COLLAPSE)}
+            ></i>
+          )}
+          {isCollapsed && (
+            <i
+              className="fa-solid fa-arrow-right"
+              onClick={() => onClickHandler(RESIZE_TYPES.EXPAND)}
+            ></i>
+          )}
         </div>
       </div>
     </div>
